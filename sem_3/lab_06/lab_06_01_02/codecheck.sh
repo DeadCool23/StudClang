a067e6e98f346a5d471664e2181f4be5be6c9132
nisuev <nisuev04@mail.ru>
2024-03-26 02:58:39 +0300

    del trash

--- sem_3/lab_06/lab_06_01_02/codecheck.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for c_file in *.c; do
-	~/apps/CodeChecker/CodeChecker/CodeChecker.exe --rules ~/apps/CodeChecker/CodeChecker/Rules.txt "$c_file"
-done
--- sem_3/lab_10/lab_10_02_01/func_test/codecheck.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for c_file in ../src/*.c; do
-	~/apps/CodeChecker/CodeChecker/CodeChecker.exe --rules ~/apps/CodeChecker/CodeChecker/Rules.txt "$c_file"
-done
